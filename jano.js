a3c90c67b08120314c6619e2b2ad523acd6d0d3e
MrRobot988 <84110291+MrRobot988@users.noreply.github.com>
2021-05-16 19:46:09 +0300

    Update jano.js

--- jano.js
+++ jano.js
@@ -7392,7 +7392,7 @@ jano200.on("ready", () => {
 
 //////////////// tokn lera da bne 
 
-jano.login("");
+jano.login("ODQzNDk5NzQwMzQzNTY2Mzk3.YKExgQ.or681sHM1HXhbeTaDcWVRbzwduM");
 jano2.login("");
 jano3.login("");
 jano4.login("");
